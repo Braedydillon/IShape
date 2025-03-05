403bd676bdb2a6de4098783e16e8fcc4fa938bc3
Braedydillon <ST10439231@vcconnect.edu.za>
2025-03-05 15:50:51 +0200

    Add project files.

---
+++ IShape/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IShape
+{
+    
+        interface IShape
+        {
+            void Draw();
+        }
+
+        class Circle : IShape
+        {
+            public void Draw()
+            {
+                Console.WriteLine("Drawing a circle");
+            }
+        }
+        class Square: IShape
+        {
+            public void Draw()
+            {
+                Console.WriteLine("Drawing a Square");
+            }
+        }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IShape shape1 = new Circle();
+            IShape shape2 = new Square();
+
+            shape1.Draw();//Calls Cricles Draw
+            shape2.Draw();//Calls Squares Draw
+        }
+    }
+}
